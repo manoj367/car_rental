b3b1e275c32b720d287bd8a48e313f10caf027ce
manoj367 <manojkumarrko72@gmail.com>
2019-04-10 23:11:02 +0530

    First commit

---
+++ resources/views/auth/about.blade.php
@@ -0,0 +1,188 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="author" content="Jewel Theme">
+        <meta name="description" content="Wheel - Responsive and Modern Car Rental Website Template">
+        <meta name="keywords" content="">
+        <title>Wheel</title>
+        <link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon">
+        <!-- reset css -->
+        <link rel="stylesheet" type="text/css" href="{{URL::to('assets/css/css_reset.css')}}">
+        <!-- bootstrap -->
+        <link rel="stylesheet" type="text/css" href="{{URL::to('assets/css/bootstrap.min.css')}}">
+        <link rel="stylesheet" type="text/css" href="{{URL::to('assets/css/jquery.datetimepicker.min.css')}}">
+        <!-- Latest compiled and minified CSS -->
+        <link rel="stylesheet" href="{{URL::to('assets/css/bootstrap-select.min.css')}}">
+        <!-- preload -->
+        <link rel="stylesheet" type="text/css" href="{{URL::to('assets/css/loaders.min.css')}}">
+        <link rel="stylesheet" type="text/css" href="{{URL::to('assets/css/index.css')}}">
+        <!--[if lt IE 9]>
+        <script src="assets/js/html5shiv.min.js"></script>
+        <script src="assets/js/respond.min.js"></script>
+        <![endif]-->
+    </head>
+    <body class="">
+        <!-- MAIN -->
+        <div class="load-wrap">
+            <div class="wheel-load">
+                <img src="images/loader.gif" alt="" class="image">
+            </div>
+        </div>
+        <div class="wheel-menu-wrap ">
+            <div class="container-fluid wheel-bg1">
+                <div class="row">
+                    <div class="col-sm-3">
+                        <div class="wheel-logo">
+                            <a href="index.php"><img src="images/logo.png" alt=""></a>
+                        </div>
+                    </div>
+
+                    <div class="col-sm-9 ">
+                        <div class="wheel-navigation">
+                            <nav id="dl-menu">
+                                <!-- class="dl-menu" -->
+                                <ul class="main-menu dl-menu">
+                                    <li class="menu-item   current-menu-parent menu-item-has-children  ">
+                                        <a href="{{url('/')}}">Home</a>
+
+                                    </li>
+                                    <li class="menu-item current-menu-parent menu-item-has-children  ">
+                                        <a href="{{route('auth_car_list')}}"> Listing </a>
+
+
+                                    </li>
+
+                                    <li class="menu-item menu-item-has-children  ">
+                                        <a href="{{route('auth_contact')}}">Contact</a>
+
+                                    </li>
+
+                                    </li>
+                                    <li class="menu-item ">
+                                        <a href="{{route('auth_about')}}">About</a>
+                                    </li>
+                                    <li class="menu-item ">
+                                        <a href="{{route('auth_account')}}">Account</a>
+                                    </li>
+                                    @if(Auth::check())
+                                        <li class="menu-item ">
+                                            <a href="{{route('user_reservation')}}">Reservation</a>
+                                        </li>
+                                        <li class="menu-item ">
+                                            <a href="{{route('user_activity')}}">Activity</a>
+                                        </li>
+                                        <li class="menu-item ">
+                                            <a href="{{route('user_logout')}}">Logout</a>
+                                        </li>
+
+                                    @endif
+                                </ul>
+                                <div class="nav-menu-icon"><i></i></div>
+                            </nav>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- ////////////////////////////////////////////////////////////// -->
+        <div class="wheel-start3">
+            <img src="images/bg7.jpg" alt="" class="wheel-img">
+            <div class="container">
+                <div class="row">
+                    <div class="col-xs-12 padd-lr0">
+                        <div class="wheel-start3-body clearfix marg-lg-t255 marg-lg-b75 marg-sm-t190 marg-xs-b30">
+                            <h3>About Us</h3>
+                            <ol class="breadcrumb">
+                                <li><a href="#">Home</a></li>
+                                <li><a href="#"> pages </a></li>
+                                <li class="active">About</li>
+                            </ol>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- /////////////////////////////////////////////////// -->
+        <div class="container padd-lr0">
+            <div class="row">
+                <div class="col-md-6 ">
+                    <div class="wheel-info-img  marg-lg-t150 marg-lg-b150 marg-md-t100 marg-md-b100">
+                        <img src="images/i7.png" alt="" class="wheel-img">
+                    </div>
+                </div>
+                <div class="col-md-6 ">
+                    <div class="wheel-info-text  marg-lg-t150 marg-lg-b150 marg-md-t100 marg-md-b100 marg-sm-t50 marg-sm-b50">
+                        <div class="wheel-header">
+                            <h5>Who we are  </h5>
+                            <h3>We Love Our <span>Customers</span></h3>
+                        </div>
+                        <span>Duis sed odio sit amet nibh vulputate cursus a sit amet mauris. Morbi accumsan ipsum velit. Nam nec tellus a odio tincidunt auctor a ornare odio. </span>
+                        <p>Sed non mauris vitae erat consequat auctor eu in elit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Mauris in erat justo. Nullam ac urna eu felis dapibus condimentum sit amet a augue. Sed non neque elit. Sed ut imperdiet nisi. Proin condimentum fermentum nunc. Etiam </p>
+                        <a href="" class="wheel-btn">Learn More</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- /////////////////////////////////////////////////////////// -->
+        <div class="wheel-bg2">
+            <div class="container">
+                <div class="row">
+                    <div class="col-xs-12">
+                        <div class="wheel-header wheel-testi-header text-center marg-lg-t145 marg-lg-b65 marg-md-t100 marg-sm-t50 marg-md-b50">
+                            <h3>Our Mission is <span>Client’s</span> Satisfaction</h3>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-xs-12">
+                        <div class="wheel-testimonial text-center">
+                            <p>Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis sem nibh id elit. Duis sed odio sit amet nibh vulputate cursus a sit amet mauris. Morbi accumsan ipsum velit. Nam nec tellus a odio tincidunt auctor a ornare odio.</p>
+                            <div class="wheel-testimonial-info">
+                                <span>Aakash</span>
+                                <img src="images/l1.png" alt="">
+                                <p>CEO and Co-founder</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+
+
+        <div class="wheel-footer-info wheel-bg6">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-8 col-sm-6  padd-lr0"><span>&#169; WHEEL 2019 | Designed with Love</span></div>
+                    <div class="col-lg-4 col-sm-6 padd-lr0">
+                        <ul>
+                            <li><a href="">Home</a></li>
+                            <li><a href=""> Pages</a></li>
+                            <li><a href=""> Listings</a></li>
+                            <li><a href=""> Reservation</a></li>
+                            <li><a href="">Location</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Scripts project -->
+        <script type="text/javascript" src="{{URL::to('assets/js/jquery-2.2.4.min.js')}}"></script>
+        <script type="text/javascript" src="{{URL::to('assets/js/bootstrap.min.js')}}"></script>
+        <!-- count -->
+        <script type="text/javascript" src='{{URL::to('assets/js/jquery.countTo.js')}}'></script>
+        <!-- google maps -->
+        <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBt5tJTim4lOO3ojbGARhPd1Z3O3CnE-C8" type="text/javascript"></script>
+        <!-- swiper -->
+        <script type="text/javascript" src="{{URL::to('assets/js/idangerous.swiper.min.js')}}"></script>
+        <script type="text/javascript" src="{{URL::to('assets/js/equalHeightsPlugin.js')}}"></script>
+        <script type="text/javascript" src="{{URL::to('assets/js/jquery.datetimepicker.full.min.js')}}"></script>
+        <!-- Latest compiled and minified JavaScript -->
+        <script type="text/javascript" src="{{URL::to('assets/js/bootstrap-select.min.js')}}"></script>
+        <script type="text/javascript" src="{{URL::to('assets/js/index.js')}}"></script>
+        <!-- sixth block end -->
+    </body>
+</html>
